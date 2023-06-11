fab04b25370ffe75b54d33879976480dfb32dccf
zkx <zhxuankun@163.com>
2023-06-11 13:27:57 +0800

    [init]finish echo challenge

---
+++ src/bin/echo.rs
@@ -0,0 +1,91 @@
+use std::io::Write;
+
+use anyhow::Context;
+use rustgen::main_loop;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+#[serde(tag = "type")]
+enum EchoMessage {
+    Echo { echo: String },
+    EchoOk { echo: String },
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct EchoNode {
+    id: usize,
+}
+
+impl rustgen::Node<EchoMessage> for EchoNode {
+    fn init_from(_: &rustgen::InitBody) -> anyhow::Result<Self>
+    where
+        Self: Sized,
+    {
+        Ok(Self { id: 1 })
+    }
+
+    fn step(
+        &mut self,
+        req: rustgen::Message<EchoMessage>,
+        output: &mut std::io::StdoutLock,
+    ) -> anyhow::Result<()> {
+        let mut msg = req.into_reply(Some(&mut self.id));
+        match msg.body.payload {
+            EchoMessage::Echo { echo } => {
+                msg.body.payload = EchoMessage::EchoOk { echo };
+                msg.serialize(&mut serde_json::Serializer::new(&mut *output))
+                    .context("serialize echo_ok message failed")?;
+                output.write_all(b"\n")?;
+            }
+            EchoMessage::EchoOk { .. } => unreachable!(),
+        }
+        Ok(())
+    }
+}
+
+fn main() -> anyhow::Result<()> {
+    main_loop::<EchoMessage, EchoNode>()?;
+    Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use std::io::Write;
+
+    use rustgen::{Body, Message};
+    use serde::Serialize;
+
+    use crate::EchoMessage;
+
+    #[test]
+    fn test_echo_node_msg() -> anyhow::Result<()> {
+        let echo_ok_msg = EchoMessage::EchoOk {
+            echo: "echo".to_string(),
+        };
+        let msg = Message {
+            src: "c1".to_string(),
+            dst: "n1".to_string(),
+            body: Body {
+                payload: echo_ok_msg,
+                id: None,
+                in_reply_to: None,
+            },
+        }
+        .into_reply(Some(&mut 1));
+        let stdout = std::io::stdout().lock();
+        let mut output = serde_json::Serializer::new(stdout);
+        msg.serialize(&mut output)?;
+        Ok(())
+    }
+
+    #[test]
+    fn test_stdout() -> anyhow::Result<()> {
+        let mut out = std::io::stdout().lock();
+        out.write(b"hello")?;
+        out.flush()?;
+        out.write(b"zxk")?;
+        // out.write_all(b"hello2")?;
+        Ok(())
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,158 @@
+use std::{
+    fmt::Debug,
+    io::{stdout, BufReader, StdoutLock, Write},
+};
+
+use anyhow::Context;
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Message<MessageType> {
+    pub src: String,
+    #[serde(rename = "dest")]
+    pub dst: String,
+    pub body: Body<MessageType>,
+}
+
+impl<M> Message<M> {
+    pub fn into_reply(self, msg_id: Option<&mut usize>) -> Self {
+        Self {
+            src: self.dst,
+            dst: self.src,
+            body: Body {
+                payload: self.body.payload,
+                id: msg_id.map(|id| {
+                    let mid = *id;
+                    *id += 1;
+                    mid
+                }),
+                in_reply_to: self.body.id,
+            },
+        }
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Body<MessageType> {
+    #[serde(rename = "msg_id")]
+    pub id: Option<usize>,
+    pub in_reply_to: Option<usize>,
+    #[serde(flatten)]
+    pub payload: MessageType,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "type")]
+#[serde(rename_all = "snake_case")]
+pub enum InitMsg {
+    Init(InitBody),
+    InitOk,
+}
+
+impl Message<InitMsg> {
+    pub fn into_init_ok(&self) -> anyhow::Result<Self> {
+        match &self.body.payload {
+            InitMsg::Init(..) => Ok(Message {
+                src: self.dst.clone(),
+                dst: self.src.clone(),
+                body: Body {
+                    id: None,
+                    in_reply_to: self.body.id,
+                    payload: InitMsg::InitOk,
+                },
+            }),
+            InitMsg::InitOk => anyhow::bail!("can't convert from init_ok messag"),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct InitBody {
+    pub node_id: String,
+    pub node_ids: Vec<String>,
+}
+
+pub trait Node<MessageType> {
+    fn init_from(init: &InitBody) -> anyhow::Result<Self>
+    where
+        Self: Sized;
+
+    fn step(&mut self, req: Message<MessageType>, output: &mut StdoutLock) -> anyhow::Result<()>;
+}
+
+pub fn main_loop<MessageType, N>() -> anyhow::Result<()>
+where
+    MessageType: DeserializeOwned,
+    N: Node<MessageType>,
+{
+    // let mut stdin = std::io::stdin().lines();
+    // let init_msg: Message<InitMsg> = serde_json::from_str(
+    //     &stdin
+    //         .next()
+    //         .expect("no init message")
+    //         .context("failed to read init msg from stdin")?,
+    // )?;
+    // let InitMsg::Init ( ref init_body ) = init_msg.body.payload else {
+    //     panic!("first message should be init.");
+    // };
+    let mut stdin = BufReader::new(std::io::stdin().lock());
+    let init_msg = serde_json::Deserializer::from_reader(&mut stdin)
+        .into_iter::<Message<InitMsg>>()
+        .next()
+        .expect("no init msg received at first")
+        .context("construct init message failed")?;
+    let InitMsg::Init ( ref init_body ) = init_msg.body.payload else {
+        panic!("first message should be init.");
+    };
+
+    let mut stdout = stdout().lock();
+    serde_json::to_writer(&mut stdout, &init_msg.into_init_ok()?)?;
+    stdout.write_all(b"\n")?;
+
+    let mut node: N =
+        Node::init_from(init_body).context("construct node from init message failed")?;
+    let stdin =
+        serde_json::Deserializer::from_reader(&mut stdin).into_iter::<Message<MessageType>>();
+    for line in stdin {
+        let msg = line.context("Maelstrom input from STDIN could not be read")?;
+        node.step(msg, &mut stdout)?;
+    }
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use serde::Serialize;
+
+    use crate::{Body, InitBody, InitMsg, Message};
+
+    #[test]
+    fn name() -> anyhow::Result<()> {
+        let init = InitMsg::Init(InitBody {
+            node_id: "n1".to_string(),
+            node_ids: vec!["n1".to_string(), "n2".to_string()],
+        });
+        let msg = Message {
+            src: "c1".to_string(),
+            dst: "n1".to_string(),
+            body: Body {
+                payload: init,
+                id: Some(1),
+                in_reply_to: Some(1),
+            },
+        };
+        let stdout = std::io::stdout().lock();
+        let mut output = serde_json::Serializer::new(stdout);
+        msg.serialize(&mut output)?;
+        Ok(())
+    }
+
+    #[test]
+    fn serde_from_str() -> anyhow::Result<()> {
+        let content = r#"{"src":"c1","dest":"n1","body":{"type":"init","node_id":"n1","node_ids":["n1","n2"],"msg_id":1,"in_reply_to":1}}"#;
+        let msg: Message<InitMsg> = serde_json::from_str(content)?;
+        println!("{msg:?}");
+        Ok(())
+    }
+}
